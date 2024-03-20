3a3edcbd638fbcb77cbf2db3259db4cf78c420b6
gabriel henrique <gabrielhenriqueas1@hotmail.com>
2024-03-20 15:55:48 -0300

    20.03.2024

--- vitalHub/src/components/CancelattionModal/CancelattionModal.js
+++ vitalHub/src/components/CancelattionModal/CancelattionModal.js
@@ -4,7 +4,51 @@ import { Button, ButtonTitle } from "../Button/Button";
 import { ButtonLinkMediumCancel, LinkMedium } from "../LinkMedium/LinkMedium";
 import { ModalContent, ModalText, PatientModal } from "./Style";
 
+//importar os recursos do expo-notifications
+import * as Notifications from "expo-notifications";
+
+//solicita permissões de notificação ao iniciar o app
+Notifications.requestPermissionsAsync();
+
+//define como as notificações devem ser tratadas quando recebidas
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+
+    //mostrar o alerta quando a notificação for recebida
+    shouldShowAlert: true,
+
+    //reproduz som ao receber notificação
+    shouldPlaySound: true,
+
+    //número de notificações no ícone do app
+    shouldSetBadge: false,
+  })
+})
+
 export const CancelattionModal = ({ visible, setShowModalCancel, ...rest }) => {
+  
+  //função para lidar com chamada de notificação
+  const handleCallNotifications = async () => {
+
+    //obtém o status da permissão 
+    const { status } = await Notifications.getPermissionsAsync();
+
+    //verifica se o usuário concedeu permissão
+    if (status !== "granted") {
+      alert("Você não deixou as notificações ativas!")
+      return;
+    }
+
+    //agenda uma notificação
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "Bem-vindo ao SENAI!",
+        body: "Notificação recebida.",
+      },
+      trigger: null
+    })
+  }
+  
   return (
     <Modal {...rest} visible={visible} transparent={true} animationType="fade">
       <PatientModal>
@@ -16,7 +60,7 @@ export const CancelattionModal = ({ visible, setShowModalCancel, ...rest }) => {
             seu horário, deseja mesmo cancelar essa consulta?
           </ModalText>
 
-          <Button onPress={() => setShowModalCancel(false)}>
+          <Button onPress={handleCallNotifications}>
             <ButtonTitle>Confirmar</ButtonTitle>
           </Button>
 
